466c9ad34ffe4d5383b8c98d17bae87342f4d6ba
basbelmar <galvanize.bas@gmail.com>
2018-07-19 21:15:07 -0400

    añadido hasta el ejercicio5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 $(function() {
   // Handler for .ready() called.
 
-  $('.card').on('click', function(){
-  		$(this).toggleClass('card--open'); 
-  });
+  $('.card').on('click', function(e){
+        $(this).toggleClass('card--open');
 });
+
+$('.card__like').click(function(e) {
+  $(this).toggleClass('card__like--red');
+          e.preventDefault();
+          e.stopPropagation();
+});
+
+$('.card__follow-btn').click(function(e){
+	  $(this).toggleClass('card__follow-btn--following');
+	  e.preventDefault();
+      e.stopPropagation();
+
+});
+
+});
+
+
+
+
 
 
